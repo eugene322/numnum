e05c00551dea0c659347b4a7388b44b964d32660
Muslim Beibytuly <beibituly.m@chocofood.kz>
2019-09-09 19:33:38 +0600

    Fix CI

--- src/core/helpers/default_apps.py
+++ src/core/helpers/default_apps.py
@@ -4,6 +4,5 @@ DEFAULT_APPS: Tuple = (
     'django.contrib.admin', 'django.contrib.auth', 'django.contrib.contenttypes',
     'django.contrib.sessions', 'django.contrib.messages',
     'django.contrib.staticfiles', 'corsheaders',
-    'rest_framework', 'django_extensions', 'drf_yasg',
-    'rest_framework_simplejwt', 'silk', 'django_filters',
+    'rest_framework', 'django_extensions', 'drf_yasg', 'silk', 'django_filters',
 )
